25dbf1543f9965ad92543e3bee31f561ec6d2846
Tuvulcan <dhruvmuk@gmail.com>
2020-01-25 16:27:52 -0500

    Home Screen so far

--- menu.py
+++ menu.py
@@ -7,7 +7,43 @@ from kivy.uix.button import Button
 #from kivy.config import config
 
 
+class Screen2(GridLayout):
+
+    def pressed(self, instance):
+        # Get the value of all of the tex inputs
+        username = self.username.text
+        password = self.password.text
+
+        # print the values to the screen
+        print("Name:", username, "Last Name:", password)
+
+        # Reset text to blank in each text input
+        self.username.text = ""
+        self.password.text = ""
+
+    def __init__(self, **kwargs):
+        super(Screen2, self).__init__(**kwargs)
+        self.cols = 1
+
+        self.title=(Label(text='[font=Helvetica]'+'Screen 2'+'[/font]', markup = True, font_size = 50))
+        self.add_widget(self.title)
+
+
+
 class LoginScreen(GridLayout):
+
+    def pressed(self, instance):
+        # Get the value of all of the tex inputs
+        username = self.username.text
+        password = self.password.text
+
+        # print the values to the screen
+        print("Name:", username, "Last Name:", password)
+
+        # Reset text to blank in each text input
+        self.username.text = ""
+        self.password.text = ""
+        return Screen2()
 
     def __init__(self, **kwargs):
         super(LoginScreen, self).__init__(**kwargs)
@@ -20,16 +56,13 @@ class LoginScreen(GridLayout):
         self.username = TextInput(multiline=False)
         self.add_widget(self.username)
 
-        self.add_widget(Label(text='[font=Helvetica]'+'Email'+'[/font]', markup = True))
-        self.email = TextInput(multiline=False)
-        self.add_widget(self.email)
-
         self.add_widget(Label(text='[font=Helvetica]'+"Password"+ '[/font]', markup = True))
         self.password = TextInput(password=True, multiline=False)
         self.add_widget(self.password)
 
         self.login = Button(text = '[font=Helvetica]' + "Log in" + '[/font]', font_size = 20, markup = True)
         self.add_widget(self.login)
+        self.login.bind(on_press=self.pressed)
 
         self.createAcc = Button(text = '[font=Helvetica]' + "Create Account" + '[/font]', font_size = 20, markup = True)
         self.add_widget(self.createAcc)
@@ -40,6 +73,7 @@ class MyApp(App):
     def build(self):
         Window.size = (500,600)
         return LoginScreen()
+        return Screen2()
 
 
 if __name__ == '__main__':
