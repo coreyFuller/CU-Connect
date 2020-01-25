f11b393b0539e938bc4e13fdad12d463e750fb0a
unknown <coreyfuller00@gmail.com>
2020-01-25 16:40:33 -0500

    Added dictionaries

--- src/cuco.py
+++ src/cuco.py
@@ -8,3 +8,78 @@ class Student:
     Discord
     totalDistance = []
     
+    
+list = []
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+classDict = {
+    "CPSC 1010" : 0,
+    "CPSC 1060" : 20
+    "CPSC 1020" : 50,
+    "CPSC 1070" : 70,
+    "CPSC 2070" : 100,
+    "CPSC 2120" : 125,
+    "CPSC 2150" : 135,
+    "CPSC 2310" : 160,
+    "CPSC 3220" : 210,
+    "CPSC 3720" : 220,
+    ""
+ }
+
+classHobbies = {
+    "Coding" :
+    "Virtual Reality" : 
+    "Video games" :
+    "Robotics" :
+    "Movies" : 
+    "Music/Music Production" :
+    "Photography" :
+    "Archery" :
+    "Basketball" :
+    "Baseball" :
+    "Track" :
+    "Cross-Country" :
+    "Volleyball" :
+    "Lacrosse" :
+    "Soccer" :
+    "Softball" :
+    "Swimming" :
+    "Football" :
+    "Rowing" :
+    "Field Hockey" :
+    "Bowling" :
+    "Golf" :
+    "Frisbee" :
+    "Ping Pong" :
+    "Cheerleading" :
+    "Fencing" :
+    "Esports" :
+    "Skateboarding" :
+    "Hockey" :
+}
