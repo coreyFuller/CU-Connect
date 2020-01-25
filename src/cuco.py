a9b6c150078a3b7d28be3183e25d1aa811127bc2
John Choi <jschoi@g.clemson.edu>
2020-01-25 16:31:24 -0500

    Messing with classes

--- src/cuco.py
+++ src/cuco.py
@@ -1,10 +1,20 @@
 class Student:
-    ID
+    ID = 1
     Hobbies = []
     Classes = []
-    Majors = []
-    email
     Name = []
-    Discord
+    unhash
+    pwhash
     totalDistance = []
+
+    # default constructor 
+    def __init__(self, id, hobbies, classes, name, hasval1, hasval2): 
+        self.ID = id
+        self.Hobbies = hobbies
+        self.Classes = classes
+        self.Name = name
+        self.unhash = hasval1
+        self.pwhash = hasval2
+        self.totalDistance = 0
+    
     
