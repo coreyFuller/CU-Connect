41d322f2fb9b6cba70d0ede2f6ff938d9c506046
Tuvulcan <dhruvmuk@gmail.com>
2020-01-25 20:48:25 -0500

    Working Window Swtiches

--- menu.py
+++ menu.py
@@ -1,80 +1,94 @@
+import kivy
 from kivy.app import App
-from kivy.core.window import Window
-from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
-from kivy.uix.textinput import TextInput
+#from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
-#from kivy.config import config
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.lang import Builder
+from kivy.uix.popup import Popup
 
 
-class Screen2(GridLayout):
 
-    def pressed(self, instance):
-        # Get the value of all of the tex inputs
-        username = self.username.text
-        password = self.password.text
 
-        # print the values to the screen
-        print("Name:", username, "Last Name:", password)
+class LoginWindow(Screen):
+    email = ObjectProperty(None)
+    password = ObjectProperty(None)
 
-        # Reset text to blank in each text input
-        self.username.text = ""
+    def btn(self):
+        print("Name:", self.name.text, "email:", self.email.text)
+        self.email.text = ""
         self.password.text = ""
 
-    def __init__(self, **kwargs):
-        super(Screen2, self).__init__(**kwargs)
-        self.cols = 1
+    def createBtn(self):
+        self.reset()
+        sm.current = "create"
 
-        self.title=(Label(text='[font=Helvetica]'+'Screen 2'+'[/font]', markup = True, font_size = 50))
-        self.add_widget(self.title)
+    def loginBtn(self):
+        self.reset()
+        sm.current="main"
 
-
-
-class LoginScreen(GridLayout):
-
-    def pressed(self, instance):
-        # Get the value of all of the tex inputs
-        username = self.username.text
-        password = self.password.text
-
-        # print the values to the screen
-        print("Name:", username, "Last Name:", password)
-
-        # Reset text to blank in each text input
-        self.username.text = ""
-        self.password.text = ""
-        return Screen2()
-
-    def __init__(self, **kwargs):
-        super(LoginScreen, self).__init__(**kwargs)
-        self.cols = 1
-
-        self.title=(Label(text='[font=Helvetica]'+'CU CONNECT'+'[/font]', markup = True, font_size = 50))
-        self.add_widget(self.title)
-
-        self.add_widget(Label(text='[font=Helvetica]'+'Username'+'[/font]', markup = True))
-        self.username = TextInput(multiline=False)
-        self.add_widget(self.username)
-
-        self.add_widget(Label(text='[font=Helvetica]'+"Password"+ '[/font]', markup = True))
-        self.password = TextInput(password=True, multiline=False)
-        self.add_widget(self.password)
-
-        self.login = Button(text = '[font=Helvetica]' + "Log in" + '[/font]', font_size = 20, markup = True)
-        self.add_widget(self.login)
-        self.login.bind(on_press=self.pressed)
-
-        self.createAcc = Button(text = '[font=Helvetica]' + "Create Account" + '[/font]', font_size = 20, markup = True)
-        self.add_widget(self.createAcc)
     
 
+    def reset(self):
+        self.email.text = ""
+        self.password.text = ""
+
+
+class CreateAccountWindow(Screen):
+    namee = ObjectProperty(None)
+    email = ObjectProperty(None)
+    password = ObjectProperty(None)
+
+    def login(self):
+        self.reset()
+        sm.current = "login"
+
+    def submit(self):
+        return subbutt()
+
+    def reset(self):
+        self.email.text = ""
+        self.password.text = ""
+        self.namee.text = ""
+
+
+class MainWindow(Screen):
+    n = ObjectProperty(None)
+    created = ObjectProperty(None)
+    email = ObjectProperty(None)
+    current = ""
+
+    def logOut(self):
+        sm:current = "login"
+
+    def on_enter(self, *args):
+        self.n.text = "Account Name: Jamie"
+        self.email.text = "Email: Ketchup@gmail.com"
+        self.created.text = "Created On: Now"
+
+def subbutt():
+    pop = Popup(title='SUBMITTED', 
+                    content=Label(text="Your thing has been submitted"), size_hint=(None,None), size=(400,300))
+    pop.open()
+
+class WindowManager(ScreenManager):
+    pass
+
+kv = Builder.load_file("my.kv")
+sm = WindowManager()
+
+screens = [LoginWindow(name="login"), CreateAccountWindow(name="create"), MainWindow(name="main")]
+for screen in screens:
+    sm.add_widget(screen)
+
+sm.current = "login"
+
 class MyApp(App):
-
     def build(self):
-        Window.size = (500,600)
-        return LoginScreen()
-        return Screen2()
+        return sm
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     MyApp().run()
