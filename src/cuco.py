a6e559851edbbbd5e55ba55153bf8724d7c174f3
unknown <coreyfuller00@gmail.com>
2020-08-19 21:28:56 -0400

    removing unecessary vars

--- src/cuco.py
+++ src/cuco.py
@@ -1,47 +1,25 @@
 import json #needed to read in JSON File
 import os 
+import inspect
 cwd = os.getcwd()
 
-print(cwd)
-with open(cwd + '\hobbies.json') as hobbies: 
+
+filename = inspect.getframeinfo(inspect.currentframe()).filename
+path = os.path.dirname(os.path.abspath(filename))
+
+print(path)
+with open(path + '\hobbies.json') as hobbies: 
     hobbyValues = json.load(hobbies)
 
-with open(cwd + '\classes.json') as classes:
+with open(path + '\classes.json') as classes:
      classValues = json.load(classes)
      
-with open(cwd + '\db.json') as studentDB:
+with open(path + '\db.json') as studentDB:
     students = json.load(studentDB)
 
+with open(path + '\db.json') as json_file:
+    data = json.load(json_file)
 
-
-class Student:
-    ID = 0
-    Hobbies = []
-    Classes = []
-    Name = " "
-    Email = " "
-    totalDistance = 0
-    matchAverage = 0
-
-    # default constructor 
-    def __init__(self, idval, name, email, hobbies, classes): 
-        self.ID = idval
-        self.Name = name
-        self.Hobbies = hobbies
-        self.Classes = classes
-        self.Email = email
-        self.totalDistance = 0
-        self.matchAverage = 0
-    def display(self):
-        print(self.ID, self.Email, self.Name, self.Hobbies, self.Classes)
-
-studentList = []
-
-#holds the similarity ratio for the all students tof
-#the one being matched
-
-#read in info from the JSON file and pass it to an array of objects
-#build objects by putting info in the constructors
 
 def calcDistances(StudentA, StudentB):
     hobbyDist = 0
@@ -59,31 +37,55 @@ def calcDistances(StudentA, StudentB):
 
 def matchStudents(students):
     #sorts objects based on average
-    students.sort(key = lambda x: x.totalDistance)
+    students.sort(key = lambda x: x.TotalDistance)
     #creates list of top 5 matches
     matched = students[:5]
     #returns list
     return matched
 
+class Student:
+    Id = 0
+    Username = ""
+    TotalDistance = 0
+    MatchAverage = 0
+    Hobbies = []
+    Classes = []
+    
+    #constructor 
+    def __init__(self, idval, name, hobbies, classes): 
+        self.Id = idval
+        self.Username = name
+        self.Hobbies = hobbies
+        self.Classes = classes
+        self.TotalDistance = 0
+        self.MatchAverage = 0
+    def display(self):
+        print(self.Id, self.Username, self.Hobbies, self.Classes)
 
-with open('db.json') as json_file:
-    data = json.load(json_file)
-    for s in data['students']:
-        studentList.append(Student(s['UID'], s['name'], s['email'], s['hobbies'], s['classes']))
+studentList = []
+
+#holds the similarity ratio for the all students tof
+#the one being matched
+
+#read in info from the JSON file and pass it to an array of objects
+#build objects by putting info in the constructors
+
+for s in data['students']:
+    studentList.append(Student(s['UID'], s['name'], s['hobbies'], s['classes']))
 matchStudent = studentList.pop()
     
     #for all students in list
     # find the total distace for all 10 dimensions from user to each student
     # store average of distances total in student object
 for x in studentList:
-    x.totalDistance += calcDistances(matchStudent, x);
+    x.TotalDistance += calcDistances(matchStudent, x);
     
     #sort based on average
     # return 10 student objects   
-print("Scott Lang: ")
+print(matchStudent.Username)
 matchStudent.display()
 print("\n")
-print("Here are " + matchStudent.Name + "'s matches: ")
+print("Here are " + matchStudent.Username + "'s matches: ")
 matched = matchStudents(studentList)  
 for x in matched:
     x.display()
